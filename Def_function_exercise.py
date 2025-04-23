25a0613271248cefeae021e66a063ac31eedca0b
Or1on <lancelot25565@gmail.com>
2025-04-23 16:28:20 +0300

    Add files via upload

---
+++ Def_function_exercise.py
@@ -0,0 +1,8 @@
+def mood(your_mood = 'happy'):
+    if your_mood == "happy":
+        print("You are happy now!")
+    else:
+        print("""You are not happy now, but don't worry, it's temporary!""")
+# Here you just have to enter 'mood() or mood("whatever")' and see the result:
+mood()
+mood("")
