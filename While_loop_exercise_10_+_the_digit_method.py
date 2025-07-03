1df4de08baab1b91ecf88c4d42c3eda3be9f0e6f
Or1on <lancelot25565@gmail.com>
2025-07-03 21:19:35 +0300

    While_loop_exercise_10_+_the_digit_method.py

--- While_loop_exercise_10_+_the_digit_method.py
+++ While_loop_exercise_10_+_the_digit_method.py
@@ -3,12 +3,14 @@
 
 # Here the program checks if the entered value is NOT a digit and the amount fo entered characters is equal to 1
 question = input('Enter a letter: ')
+
 while len(question) == 1 and not question.isdigit() or question:
     if not question.isdigit() and len(question) == 1:
         print("""You entered a letter
 The program terminated
 Thanks for using!""")
         break
+        
     else:
         print("You entered smth else!")
         question = input('Enter a letter: ')
