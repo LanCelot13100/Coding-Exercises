ad3153077d990a706afba9eee23c4b68303b7551
Or1on <lancelot25565@gmail.com>
2025-07-07 23:03:54 +0300

    Def_function_exercise_2.py

--- Def_function_exercise_2.py
+++ Def_function_exercise_2.py
@@ -4,11 +4,14 @@ first = input("Enter the first name: ")
 second = input("Enter the second name: ")
 third = input("Enter the third name: ")
 surname = input("Enter the united surname for entered three names: ")
+
+
 def name(fname):
     print(f'{fname} {surname}')
 print("Here are three entered names with the united entered surname:")
 name(first)
 name(second)
 name(third)
+
 print("""The program has been terminated
 Thanks for using!""")
